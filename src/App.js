787b43b745520c3913ea910a0bb084d2b8bdb6d3
Spirovanni <blackshieldsx@gmail.com>
2022-02-04 20:17:24 -0800

    Dishes list

--- src/App.js
+++ src/App.js
@@ -26,6 +26,14 @@ function Footer(props) {
     );
 }
 
+const dishes = [
+    "Macaroni and Cheese",
+    "Salmon",
+    "Tofu with Vegetables"
+]
+
+dishes.map((dish) => console.log(dish));
+
 function App() {
   return (
     <div className="App">
